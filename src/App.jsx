f2237d073442c25c0079d8d32a3e2dc1fd5c10ac
SimraanK <simraankhatib9@gmail.com>
2023-06-10 00:41:29 +0530

    add security

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_OPEN_AI_KEY,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
 
   const openai = new OpenAIApi(configuration);
