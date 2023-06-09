0b24200b27c892f4078a12e42441f29ce9b0a111
SimraanK <simraankhatib9@gmail.com>
2023-06-10 01:09:17 +0530

    Environment Variables

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    'import.meta.env.VITE_OPENAI_API_KEY': JSON.stringify(process.env.VITE_OPENAI_API_KEY || ''),
+  },
 })
+
